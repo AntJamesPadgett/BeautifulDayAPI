bebe346baa188ca38a360477f1f404e62fbdd6d2
AntJamesPadgett <Padgettjanthony@gmail.com>
2022-03-05 11:44:35 -0600

    Completed Project

---
+++ BeautifulDayAPI/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace BeautifulDayAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            Console.WriteLine("Please enter your API key: ");
+
+            var api_key = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please enter the city name: ");
+                var city_name = Console.ReadLine();
+                var weatherURL = $"http://api.openweathermap.org/data/2.5/weather?q={city_name}&units=imperial&appid={api_key}";
+
+
+
+                var reponse = client.GetStringAsync(weatherURL).Result;
+                var formattedResponse = JObject.Parse(reponse).GetValue("main").ToString();
+
+                Console.WriteLine(formattedResponse);
+                Console.WriteLine();
+                Console.WriteLine("Would you like to choose another city?");
+                var userInput = Console.ReadLine();
+                Console.WriteLine();
+
+                if (userInput.ToLower()== "no")
+                {
+                    break;
+                }
+
+
+
+
+
+            }
+        }
+    }
+}
